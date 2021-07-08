7334727e282766df6fc54cb6d3b9b476fe2c0fbc
Erik Ramon Conan Mazon <erikramonconan.mazo@learnet.se>
2021-07-08 11:24:58 +0200

    New chapter 8 (5 for students)

---
+++ Chap5/terminal.js
@@ -0,0 +1,8 @@
+//Den här gör att vi använder Express
+var express = require ('express'); 
+const { get } = require('http');
+//variabeln app syftar till Express
+var app = express(); 
+
+//Få information från en API som redan finns på internet
+get.
