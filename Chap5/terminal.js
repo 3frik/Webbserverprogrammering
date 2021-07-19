b496a2656a69e7c506691a607c20bf008e19cdfa
Erik Ramon Conan Mazon <erikramonconan.mazo@learnet.se>
2021-07-19 08:52:18 +0200

    Chap 6(9 in book) about cookies

--- Chap5/terminal.js
+++
@@ -1,8 +0,0 @@
-//Den här gör att vi använder Express
-var express = require ('express'); 
-const { get } = require('http');
-//variabeln app syftar till Express
-var app = express(); 
-
-//Få information från en API som redan finns på internet
-get.
